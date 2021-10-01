451270d9449c0dd880e54e9cf87009e80e601800
KDY <merry4739@naver.com>
2021-10-01 12:51:33 +0900

    10월 1일 오전수업

--- 01_데이터 가져오기.sql
+++ 01_데이터 가져오기.sql
@@ -1,16 +1,65 @@
--- �ּ� ����Ű CTRL + /
+-- 주석 단축키 CTRL + /
 /*
-������
-�ּ�
+여러줄
+주석
 */
--- 1. ���̺��� ��� ��� ���� �о���� , ���ɹ� ������ CTRL + ENTER
--- * �� ��ü �� , ���ɹ� ������ �����ݷ�(;)�� �־�� �Ѵ�.
+-- 1. 테이블의 모든 행과 열을 읽어오기 , 명령문 실행은 CTRL + ENTER
+-- * 은 전체 열 , 명령문 끝에는 세미콜론(;)이 있어야 한다.
 select * 
 from departments;
 
--- 2. Ư�� ���� ��ȸ�ϱ�
+-- 2. 특정 열만 조회하기
 SELECT department_id, department_name
 FROM departments;
 
+-- 3. 별칭 붙이기 (열의 이름)
+SELECT department_id AS 부서번호, department_name 부서이름
+FROM departments;
 
+-- 4. 산술 연산자(+,-,*,/)
+SELECT first_name 이름, job_id 직책, salary 월급
+FROM employees;
+-- 월급에 100씩 뺀 열을 만들기
+SELECT first_name 이름, job_id 직책, salary 월급, salary-100 월급수정
+FROM employees;
+-- 월급에 10%를 빼기
+SELECT first_name 이름, job_id 직책, salary 월급, salary-salary/10 월급수정
+FROM employees;
 
+-- 보너스를 주는데 salary에 1000을 뺀 값의 5%로 책정
+SELECT
+    last_name,
+    first_name,
+    job_id,
+    salary,
+    (salary-1000)*0.05 as 보너스
+FROM employees;
+
+-- distinct : 중복 값을 제거 !
+select distinct job_id
+from employees;
+
+-- 예제 1
+SELECT employee_id, first_name, last_name
+FROM employees;
+
+-- 예제 2
+SELECT first_name, salary, salary*1.1 as 뉴셀러리
+FROM employees;
+
+-- 예제 3
+SELECT employee_id 사원번호, first_name 이름, last_name 성
+FROM employees;
+
+-- 연결 연산자 || : 열을 붙여서 하나의 열로 출력
+SELECT LAST_NAME || ' is a ' || JOB_ID as 직업정보
+from employees;
+
+-- 예제 4 : 이름을 한칸 띄우고 붙여서 열, eamil에 문자열 붙여서 출력
+SELECT EMPLOYEE_ID, FIRST_NAME || ' ' || LAST_NAME 풀네임, EMAIL|| '@company.com'
+FROM employees;
+
+-- describe(혹은 desc) : 열의 정보를 확인할 수 있다.
+-- (VARCHAR2는 문자를 의미하고 괄호 안의 숫자에서 첫번째는 숫자의 수, 두번째는 소숫점의 수이다.)
+DESC EMPLOYEES;
+DESC DEPARTMENTS;
---
+++ 02_Where절 사용하기.sql
@@ -0,0 +1,109 @@
+-- where 절
+SELECT *
+FROM EMPLOYEES
+WHERE SALARY > 14000; -- 월급이 14000보다 많은 직원
+
+SELECT *
+FROM EMPLOYEES
+WHERE LAST_NAME = 'King'; -- 문자열은 ''로 표시, 이름이 King인 직원
+
+SELECT *
+FROM EMPLOYEES
+WHERE HIRE_DATE < '02/06/10'; -- 날짜를 비교, 고용일이 2002년 6월 10일 이전
+
+-- 비교연산자
+-- = 같다      <>( != ) 같지 않다      > 보다 크다     >= 보다 크거나 같다
+-- < 보다 작다  <= 보다 작거나 같다
+
+-- 예제 1
+SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID = 100;
+
+-- 예제 2
+SELECT * FROM EMPLOYEES WHERE FIRST_NAME = 'David';
+
+-- 예제 3
+SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID <= 105;
+
+-- 예제 4
+SELECT * FROM JOB_HISTORY WHERE START_DATE > '06/03/03';
+
+-- 예제 5
+SELECT * FROM DEPARTMENTS WHERE LOCATION_ID <> 1700;
+
+-- 논리 연산자 AND OR NOT
+SELECT last_name, DEPARTMENT_ID, salary
+FROM EMPLOYEES
+WHERE DEPARTMENT_ID = 60
+    OR (DEPARTMENT_ID = 80
+    and SALARY > 10000);
+    
+SELECT *
+FROM employees
+WHERE NOT ( hire_date > '2004/01/01' or salary > 5000 );
+-- hire_date <= '2004/01/01' and salary <= 5000
+
+-- 예제 1
+SELECT *
+FROM employees
+WHERE salary > 4000 and job_id = 'IT_PROG';
+
+-- 예제 2
+SELECT *
+FROM employees
+WHERE salary > 4000 and (job_id = 'IT_PROG' OR job_id = 'FI_ACCOUNT');
+
+-- IN 연산자 ()안의 값과 같을때 OR로 연결
+SELECT *
+FROM employees
+WHERE salary IN ( 4000, 3000, 2700 );
+
+-- 예제 1
+SELECT * FROM employees WHERE salary IN ( 10000, 17000, 24000 );
+
+-- 예제 2
+SELECT * FROM employees WHERE department_id NOT IN ( 30, 50, 80, 100, 110 );
+
+-- BETWEEN 연산자 (사이값)
+SELECT * FROM employees WHERE salary BETWEEN 9000 AND 10000;
+
+-- 예제 1
+SELECT * FROM employees WHERE salary BETWEEN 10000 AND 20000;
+
+-- 예제 2
+SELECT * FROM employees WHERE hire_date BETWEEN '2004/01/01' and '2004/12/30';
+
+-- 예제 3 (NOT BETWEEN은 사이값의 반대)
+SELECT * FROM employees WHERE salary NOT BETWEEN 7000 AND 17000;
+
+-- LIKE 연산자(~와 같다) : %('모든문자'라는 의미)와 _('한글자'라는 의미)를 같이 사용해서 문자열을 검색
+SELECT * FROM employees WHERE last_name LIKE 'B%'; -- 대문자B로 시작 뒤에는 상관없음
+SELECT * FROM employees WHERE last_name LIKE '%B%'; -- 문자열의 중간이 B가 있으면 전부 출력
+SELECT * FROM employees WHERE last_name LIKE '____y'; -- 언더바(_) 정확한 자릿수를 표현
+
+-- 예제 1
+SELECT * FROM employees WHERE job_id LIKE '%AD%';
+
+-- 예제 2
+SELECT * FROM employees WHERE job_id LIKE '%AD___';
+
+-- 예제 3
+SELECT * FROM employees WHERE phone_number LIKE '%1234';
+
+-- 예제 4 AND나 OR연산자를 사용하는 경우에는 뒤쪽의 조건에도 검색할 열을 표시해야 한다
+SELECT * FROM employees WHERE phone_number NOT LIKE '%3%' AND phone_number LIKE '%9';
+
+-- 예제 5
+SELECT * FROM employees WHERE job_id LIKE '%MGR%' OR job_id LIKE '%ASST%';
+
+-- IS NULL 연산자
+SELECT commission_pct FROM employees;
+-- MULL 이란?
+-- 입력되지 않은 사용할 수 없는 값이고 0 또는 공백('')이 아니다.
+-- NULL값을 검색하기 위해 is NULL 을 사용
+SELECT * FROM employees WHERE commission_pct is NULL;
+-- 만약 NULL값이 아닐 경우만 찾을때 in NOT NULL 사용
+SELECT * FROM employees WHERE commission_pct is NOT NULL;
+
+-- 예제 1
+SELECT * FROM employees WHERE manager_id is NULL;
+
