e855ecac16908550cd59f470b42eb84218120e16
CBHwang00 <daydreamer00@naver.com>
2021-10-05 12:42:17 +0900

    10월 5일 오전수업

--- 03_단일행 함수.sql
+++ 03_단일행 함수.sql
@@ -44,3 +44,110 @@ SELECT employee_id 짝수번째, last_name
 FROM employees
 WHERE MOD(employee_id, 2) = 0 -- 짝수
 ORDER BY employee_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 예제 1, 2
+SELECT last_name 이름, salary 월급, NVL(commission_pct,0) 커미션,
+        salary*12 + salary*12*NVL(commission_pct, 0) 연봉
+from EMPLOYEES order by 연봉 desc;
+
+SELECT last_name 이름, salary 월급, NVL(commission_pct,0) 커미션,
+        salary*12 + salary*12*NVL(commission_pct, 0) 연봉,
+        NVL2(commission_pct, '월급+보너스', '월급만') 연봉계산방법
+from EMPLOYEES order by 연봉 desc;
+
+-- decode 함수(비교할 값이 같을 경우에 사용)
+SELECT last_name 이름, job_id, salary,
+    decode(job_id, 'IT_PROG', salary * 1.10,
+                   'ST_CLERK', salary * 1.15,
+                   'SA_REP', salary * 1.20,
+                             salary) 수정월급
+from EMPLOYEES;
+
+-- 예제
+SELECT last_name 이름, job_id 직무, salary 월급,
+       decode( trunc(salary/2000), 0, 0,
+                                   1, 0.09,
+                                   2, 0.2,
+                                   3, 0.3,
+                                   4, 0.4,
+                                   5, 0.42,
+                                   6, 0.44,
+                                      0.45) 세율
+FROM employees;
+
+-- CASE 함수 비교가능 
+SELECT last_name 이름, job_id, salary,
+    CASE WHEN salary<5000   THEN 'Low'
+         WHEN salary<10000  THEN 'Medium'
+         WHEN salary<20000  THEN 'Good'
+         ELSE                    'Excellent'
+    END "급여수준"
+FROM EMPLOYEES;
+
+-- 예제
+SELECT employee_id, first_name, last_name, salary,
+    CASE WHEN salary>=9000   THEN '상위급여'
+         WHEN salary>=6000   then '중위급여'
+         else                     '하위급여'
+    end "급여등급"
+FROM employees WHERE job_id='IT_PROG';
+
+
